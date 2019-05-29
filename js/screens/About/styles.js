ac9c58ba1226676985d9fe167da7144518b15e9a
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-28 20:32:06 -0700

    Remove styling

--- js/screens/About/styles.js
+++ js/screens/About/styles.js
@@ -1,12 +1,5 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
-});
+const styles = StyleSheet.create({});
 
 export default styles;
