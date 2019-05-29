60695a9616eac4fe2290861f950582f15bfa0a47
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-29 09:22:34 -0700

    Markup for about page and code of conducts

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -1,14 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text, View } from "react-native";
+import { Text, ScrollView, View } from "react-native";
+import CodeOfConduct from "../../components/CodeOfConduct";
 import styles from "./styles";
 
 const About = ({ allConducts }) => {
-  console.log(allConducts);
   return (
-    <View>
-      <Text>This is the about screen...</Text>
-    </View>
+    <ScrollView>
+      <Text>&lt;R10&gt;</Text>
+      <Text>
+        R10 s a conference that focuses on just about any topic related to dev.
+      </Text>
+      <Text> Date &amp; Revenue</Text>
+      <Text>
+        The R10 conference will take place on Tuestday, June 27, 2017 in
+        Vancouver,BC.
+      </Text>
+      <Text>Code of Conduct</Text>
+      {allConducts.map(conduct => (
+        <CodeOfConduct key={conduct.id} conduct={conduct} />
+      ))}
+
+      <Text>&copy; Red Academy 2019</Text>
+    </ScrollView>
   );
 };
 
