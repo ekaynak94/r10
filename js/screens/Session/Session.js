6a30d356f831fb00a65def292846812b60bc5b7c
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-29 00:47:13 -0700

    Add markup for session content

--- js/screens/Session/Session.js
+++ js/screens/Session/Session.js
@@ -1,13 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text, View } from "react-native";
+import { Text, View, TouchableHighlight, Image } from "react-native";
 import styles from "./styles";
+import moment from "moment";
 
 const Session = ({ sessionData }) => {
   console.log(sessionData);
   return (
     <View>
-      <Text>This is the Session screen...</Text>
+      <Text>{sessionData.location}</Text>
+      <Text>{sessionData.title}</Text>
+      <Text>{moment(sessionData.startTime).format("LT")}</Text>
+      <Text>{sessionData.description}</Text>
+      <Text>Presented by:</Text>
+      <TouchableHighlight
+        underlayColor="#EEEFFF"
+        onPress={() => {
+          console.log("pressed");
+        }}
+      >
+        <View>
+          <Image source={{ uri: sessionData.speaker.image }} />
+          <Text>{sessionData.speaker.name}</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 };
