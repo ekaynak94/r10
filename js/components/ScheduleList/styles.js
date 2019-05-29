9b2128fe71122d3bf5afe48fc5255ae438ef97e4
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-28 23:11:39 -0700

    Style separator

--- js/components/ScheduleList/styles.js
+++ js/components/ScheduleList/styles.js
@@ -1,5 +1,11 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  separator: {
+    width: "100%",
+    backgroundColor: "grey",
+    height: 1
+  }
+});
 
 export default styles;
