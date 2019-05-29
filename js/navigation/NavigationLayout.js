9eb838ba80020447d4001006630023c39f5dc7c8
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-28 20:23:43 -0700

    Add header with lineargradient background

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -3,25 +3,47 @@ import {
   createStackNavigator,
   createBottomTabNavigator
 } from "react-navigation";
+import { sharedNavigationOptions } from "./config";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AboutScreen from "../screens/About";
 import ScheduleScreen from "../screens/Schedule";
 import SessionScreen from "../screens/Session";
 import FavesScreen from "../screens/Faves";
 
-const AboutStack = createStackNavigator({
-  About: AboutScreen
-});
+const AboutStack = createStackNavigator(
+  {
+    About: AboutScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
 
-const ScheduleStack = createStackNavigator({
-  Schedule: ScheduleScreen,
-  Session: SessionScreen
-});
+const ScheduleStack = createStackNavigator(
+  {
+    Schedule: ScheduleScreen,
+    Session: SessionScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
 
-const FavesStack = createStackNavigator({
-  Faves: FavesScreen,
-  Session: SessionScreen
-});
+const FavesStack = createStackNavigator(
+  {
+    Faves: FavesScreen,
+    Session: SessionScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
 
 export default createBottomTabNavigator(
   {
---
+++ js/navigation/config.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Header } from "react-navigation";
+import LinearGradient from "react-native-linear-gradient";
+
+const GradientHeader = props => (
+  <View style={{ backgroundColor: "white", overflow: "hidden" }}>
+    <LinearGradient
+      colors={["#cf392a", "#9963ea"]}
+      start={{ x: 0.0, y: 1.0 }}
+      end={{ x: 1.0, y: 0.0 }}
+      style={[StyleSheet.absoluteFill, { height: 64, width: "100%" }]}
+    />
+    <Header {...props} />
+  </View>
+);
+
+export const sharedNavigationOptions = navigation => ({
+  headerBackTitle: null,
+  header: props => <GradientHeader {...props} />,
+  headerStyle: {
+    backgroundColor: "transparent"
+  }
+});
