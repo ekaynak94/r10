a5cdb3559dd88b10f7046912f6bf5841cbb00f2c
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-29 11:06:32 -0700

    Add methods to remove and add faves to provider

--- js/context/FavesContext/FavesContext.js
+++ js/context/FavesContext/FavesContext.js
@@ -1,16 +1,59 @@
 import React, { createContext, Component } from "react";
+import { queryFaves, createFave, removeFave } from "../../config/models";
 
 const FavesContext = createContext();
 
 class FavesProvider extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = { faveIds: [] };
   }
 
+  componentDidMount() {
+    this.getFavedSessionIds();
+  }
+
+  getFavedSessionIds = async () => {
+    try {
+      const faves = await queryFaves();
+      const faveIds = faves.map(fave => fave[0]);
+      console.log(faveIds);
+      this.setState({ faveIds });
+    } catch (e) {
+      return false;
+    }
+  };
+
+  addFaveSession = async sessionId => {
+    try {
+      const newFave = await createFave(sessionId);
+      if (newFave) {
+        this.setState({ faveIds: [...this.state.faveIds, newFave.id] });
+      }
+    } catch (e) {
+      return false;
+    }
+  };
+
+  removeFaveSession = async sessionId => {
+    try {
+      await removeFave(sessionId);
+      this.getFavedSessionIds();
+    } catch (e) {
+      return false;
+    }
+  };
+
   render() {
+    console.log(this.state.faveIds);
     return (
-      <FavesContext.Provider value={{ ...this.state }}>
+      <FavesContext.Provider
+        value={{
+          ...this.state,
+          addFaveSession: this.addFaveSession,
+          removeFaveSession: this.removeFaveSession
+        }}
+      >
         {this.props.children}
       </FavesContext.Provider>
     );
