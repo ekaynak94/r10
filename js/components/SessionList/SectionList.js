0b170cfcefd89e364832cb4862f1cd481f2aaa45
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-29 11:34:06 -0700

    Use section list instead of flat list

--- js/components/SessionList/SectionList.js
+++ js/components/SessionList/SectionList.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { FlatList, View, Text } from "react-native";
+import { SectionList, View, Text } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
 import moment from "moment";
 import ScheduleList from "../ScheduleList";
 
 const SessionList = ({ allSessions }) => {
-  console.log(allSessions);
   return (
     <View>
-      <FlatList
-        data={allSessions}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{moment(item.title).format("LT")}</Text>
-            <ScheduleList sessions={item.data} />
-          </View>
+      <SectionList
+        renderItem={({ item, index, section }) => (
+          <ScheduleList sessions={item} />
         )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text>{moment(title).format("LT")}</Text>
+        )}
+        sections={allSessions}
         keyExtractor={(item, index) => item.title}
       />
     </View>
