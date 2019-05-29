ccce211fd99c1f3e9eed1d5f5150af70421f6a0b
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-29 07:16:52 -0700

    Add markup for speaker modal

--- js/screens/Speaker/Speaker.js
+++ js/screens/Speaker/Speaker.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, Linking, TouchableHighlight } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
 
+const goToUrl = url => {
+  Linking.canOpenURL(url)
+    .then(supported => {
+      if (!supported) {
+        console.log("Can't handle url: " + url);
+      } else {
+        return Linking.openURL(url);
+      }
+    })
+    .catch(err => console.error("An error occurred", err));
+};
+
 const Speaker = ({ speakerData }) => {
-  console.log(speakerData);
   return (
     <View>
-      <Text>This is the Speaker screen...</Text>
+      <Image source={{ uri: speakerData.image }} />
+      <Text>{speakerData.name}</Text>
+      <Text>{speakerData.bio}</Text>
+      <TouchableHighlight
+        underlayColor="#EEEFFF"
+        onPress={() => {
+          goToUrl(speakerData.url);
+        }}
+      >
+        <Text>See more about the speaker on Wikipedia</Text>
+      </TouchableHighlight>
     </View>
   );
 };
