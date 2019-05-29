b95599a2c7cc9eaa5467ea4593b912f0567bf2ec
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-28 21:34:07 -0700

    Create dir for helper functions

---
+++ js/helpers/index.js
@@ -0,0 +1,11 @@
+export const formatSessionData = sessions => {
+  return sessions
+    .reduce((acc, curr) => {
+      const timeExists = acc.find(section => section.title === curr.startTime);
+      timeExists
+        ? timeExists.data.push(curr)
+        : acc.push({ title: curr.startTime, data: [curr] });
+      return acc;
+    }, [])
+    .sort((a, b) => a.title - b.title);
+};
