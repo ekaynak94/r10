144ff5b2f554d4681701580a9b08ed0fce1ad81a
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-28 22:15:29 -0700

    Create section list component

---
+++ js/components/SectionList/SectionList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FlatList, View, Text } from "react-native";
+import styles from "./styles";
+
+const SectionList = () => {
+  return <View />;
+};
+
+export default SectionList;
