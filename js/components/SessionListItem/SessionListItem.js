57cec68d506bed6fe48299dae20408068114725d
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 14:31:21 -0700

    Style component

--- js/components/SessionListItem/SessionListItem.js
+++ js/components/SessionListItem/SessionListItem.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Platform, View, Text, TouchableHighlight } from "react-native";
+import { Platform, View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
 import { withNavigation } from "react-navigation";
 import Icon from "react-native-vector-icons/Ionicons";
+import theme from "../../config/styles";
 
 const HeartIcon = Platform.select({
   ios: "ios-heart",
@@ -12,26 +13,29 @@ const HeartIcon = Platform.select({
 
 const SessionListItem = ({ session, navigation, faved }) => {
   return (
-    <TouchableHighlight
-      underlayColor="#EEEFFF"
+    <TouchableOpacity
       onPress={() => {
         navigation.navigate("Session", {
           session: session
         });
       }}
     >
-      <View>
-        <Text>{session.title}</Text>
-        <Text>{session.location}</Text>
-        {faved && <Icon name={HeartIcon} size={20} color={"red"} />}
+      <View style={styles.root}>
+        <Text style={styles.title}>{session.title}</Text>
+        <View style={styles.content}>
+          <Text style={styles.location}>{session.location}</Text>
+          {faved && (
+            <Icon name={HeartIcon} size={15} color={theme.colors.red} />
+          )}
+        </View>
       </View>
-    </TouchableHighlight>
+    </TouchableOpacity>
   );
 };
 
 SessionListItem.propTypes = {
-  session: PropTypes.object,
-  faved: PropTypes.bool
+  session: PropTypes.object.isRequired,
+  faved: PropTypes.bool.isRequired
 };
 
 export default withNavigation(SessionListItem);
--- js/components/SessionListItem/styles.js
+++ js/components/SessionListItem/styles.js
@@ -1,5 +1,28 @@
 import { StyleSheet } from "react-native";
+import theme from "../../config/styles";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    padding: 5
+  },
+  title: {
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize * 1.2,
+    paddingBottom: 2,
+    paddingTop: 2
+  },
+  content: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 2,
+    paddingTop: 2
+  },
+  location: {
+    color: theme.colors.mediumGrey,
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize
+  }
+});
 
 export default styles;
