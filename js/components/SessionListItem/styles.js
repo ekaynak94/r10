7170d5d60fd6da439ce25118405b8373dc11de5f
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 15:10:48 -0700

    Adjust padding

--- js/components/SessionListItem/styles.js
+++ js/components/SessionListItem/styles.js
@@ -3,20 +3,17 @@ import theme from "../../config/styles";
 
 const styles = StyleSheet.create({
   root: {
-    padding: 5
+    padding: 12
   },
   title: {
     fontFamily: theme.fonts.fontFamily,
     fontSize: theme.fonts.fontSize * 1.2,
-    paddingBottom: 2,
-    paddingTop: 2
+    paddingBottom: theme.fonts.fontSize * 0.5
   },
   content: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between",
-    paddingBottom: 2,
-    paddingTop: 2
+    justifyContent: "space-between"
   },
   location: {
     color: theme.colors.mediumGrey,
