8bc00ec08c07ff12df87d0f3eeba3278a357be85
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 16:18:57 -0700

    Style screen

--- js/screens/Session/Session.js
+++ js/screens/Session/Session.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Platform, Text, View, TouchableHighlight, Image } from "react-native";
+import {
+  Platform,
+  Text,
+  View,
+  ScrollView,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 import styles from "./styles";
 import moment from "moment";
 import { withNavigation } from "react-navigation";
 import Icon from "react-native-vector-icons/Ionicons";
+import theme from "../../config/styles";
 
 const HeartIcon = Platform.select({
   ios: "ios-heart",
@@ -14,39 +24,63 @@ const HeartIcon = Platform.select({
 const Session = ({ sessionData, faveContext, navigation }) => {
   const faved = !!faveContext.faveIds.find(id => id === sessionData.id);
   return (
-    <View>
-      <Text>{sessionData.location}</Text>
-      {faved && <Icon name={HeartIcon} size={20} color={"red"} />}
-      <Text>{sessionData.title}</Text>
-      <Text>{moment(sessionData.startTime).format("LT")}</Text>
-      <Text>{sessionData.description}</Text>
+    <ScrollView style={styles.root}>
+      <View style={styles.locationContainer}>
+        <Text style={styles.location}>{sessionData.location}</Text>
+        {faved && <Icon name={HeartIcon} size={15} color={theme.colors.red} />}
+      </View>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{sessionData.title}</Text>
+        <Text style={styles.time}>
+          {moment(sessionData.startTime).format("LT")}
+        </Text>
+        <Text style={styles.text}>{sessionData.description}</Text>
+      </View>
+
       {sessionData.speaker && (
-        <View>
-          <Text>Presented by:</Text>
-          <TouchableHighlight
-            underlayColor="#EEEFFF"
-            onPress={() => {
-              navigation.navigate("Speaker", { speaker: sessionData.speaker });
-            }}
-          >
-            <View>
-              <Image source={{ uri: sessionData.speaker.image }} />
-              <Text>{sessionData.speaker.name}</Text>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight
-            underlayColor="#EEEFFF"
-            onPress={() => {
-              faved
-                ? faveContext.removeFaveSession(sessionData.id)
-                : faveContext.addFaveSession(sessionData.id);
-            }}
-          >
-            <Text>{faved ? "Remove from Faves" : "Add to Faves"}</Text>
-          </TouchableHighlight>
+        <View style={styles.speakerContainer}>
+          <Text style={styles.speaker}>Presented by:</Text>
+
+          <View style={styles.profile}>
+            <Image
+              style={styles.avatar}
+              source={{ uri: sessionData.speaker.image }}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Speaker", {
+                  speaker: sessionData.speaker
+                });
+              }}
+            >
+              <Text style={styles.name}>{sessionData.speaker.name}</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.separator} />
+          <View style={styles.buttonContainer}>
+            <TouchableHighlight
+              underlayColor={theme.colors.lightGrey}
+              onPress={() => {
+                faved
+                  ? faveContext.removeFaveSession(sessionData.id)
+                  : faveContext.addFaveSession(sessionData.id);
+              }}
+            >
+              <LinearGradient
+                colors={[theme.colors.purple, theme.colors.red]}
+                start={{ x: 0.0, y: 1.0 }}
+                end={{ x: 1.0, y: 0.0 }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>
+                  {faved ? "Remove from Faves" : "Add to Faves"}
+                </Text>
+              </LinearGradient>
+            </TouchableHighlight>
+          </View>
         </View>
       )}
-    </View>
+    </ScrollView>
   );
 };
 
--- js/screens/Session/styles.js
+++ js/screens/Session/styles.js
@@ -1,7 +1,95 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import theme from "../../config/styles";
 
 const styles = StyleSheet.create({
-
+  root: {
+    padding: 12
+  },
+  locationContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 8,
+    marginTop: 8
+  },
+  location: {
+    color: theme.colors.mediumGrey,
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize
+  },
+  title: {
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize * 1.5,
+    marginBottom: 8,
+    marginTop: 8
+  },
+  time: {
+    color: theme.colors.red,
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize * 1.2,
+    marginBottom: 8,
+    marginTop: 8
+  },
+  text: {
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize * 1.2,
+    fontWeight: "200",
+    marginBottom: 8,
+    marginTop: 8
+  },
+  speaker: {
+    color: theme.colors.mediumGrey,
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize,
+    marginBottom: 8,
+    marginTop: 8
+  },
+  profile: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 8,
+    marginTop: 8
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginRight: 10,
+    marginLeft: 10
+  },
+  name: {
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize
+  },
+  separator: {
+    backgroundColor: theme.colors.lightGrey,
+    width: "100%",
+    height: 1,
+    marginBottom: 8,
+    marginTop: 8
+  },
+  buttonContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: 150,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 3,
+    marginTop: 3,
+    padding: 10,
+    borderRadius: 20
+  },
+  buttonText: {
+    fontFamily: theme.fonts.fontFamily,
+    fontSize: theme.fonts.fontSize,
+    color: "white"
+  }
 });
-  
+
 export default styles;
