31d42d8be02e95468ce1692702193a7f59d86147
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 22:53:09 -0700

    Increate linear gradeint height to cover entire header and color status bar

--- js/navigation/config.js
+++ js/navigation/config.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Platform, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar
+} from "react-native";
 import { Header } from "react-navigation";
 import LinearGradient from "react-native-linear-gradient";
 import Icon from "react-native-vector-icons/Ionicons";
@@ -10,9 +16,10 @@ const GradientHeader = props => (
       colors={["#cf392a", "#9963ea"]}
       start={{ x: 0.0, y: 1.0 }}
       end={{ x: 1.0, y: 0.0 }}
-      style={[StyleSheet.absoluteFill, { height: 64, width: "100%" }]}
+      style={[StyleSheet.absoluteFill, { height: "100%", width: "100%" }]}
     />
     <Header {...props} />
+    <StatusBar barStyle="light-content" />
   </View>
 );
 
