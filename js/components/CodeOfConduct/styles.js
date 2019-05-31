34d91ee8cbbaba8b0d1aac6cf36cafeef10af7cb
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 22:30:13 -0700

    styling for code of conduct

--- js/components/CodeOfConduct/styles.js
+++ js/components/CodeOfConduct/styles.js
@@ -1,5 +1,21 @@
 import { StyleSheet } from "react-native";
+import theme from "../../config/styles";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemTitle: {
+    fontSize: theme.fonts.fontSize,
+    fontFamily: theme.fonts.fontFamily,
+    color: theme.colors.purple,
+    marginBottom: 8,
+    marginTop: 8
+  },
+  itemDescription: {
+    fontFamily: theme.fonts.fontFamily,
+    fontWeight: "200",
+    fontSize: theme.fonts.fontSize,
+    marginBottom: 8,
+    marginTop: 8
+  }
+});
 
 export default styles;
