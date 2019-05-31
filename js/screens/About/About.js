a5573f67e7e3f046fc7efddf75327475514653b0
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-30 21:04:30 -0700

    Adjust mark up to style image

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -1,27 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text, ScrollView, View } from "react-native";
+import { Text, ScrollView, View, Image } from "react-native";
 import CodeOfConduct from "../../components/CodeOfConduct";
 import styles from "./styles";
 
 const About = ({ allConducts }) => {
   return (
-    <ScrollView>
-      <Text>&lt;R10&gt;</Text>
-      <Text>
+    <ScrollView contentContainerStyle={styles.root}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="center"
+          style={styles.logo}
+          source={require("../../assets/images/r10_logo.png")}
+        />
+      </View>
+
+      <View style={styles.separator} />
+      <Text style={styles.text}>
         R10 s a conference that focuses on just about any topic related to dev.
       </Text>
-      <Text> Date &amp; Revenue</Text>
-      <Text>
+      <Text style={styles.title}> Date &amp; Revenue</Text>
+      <Text style={styles.text}>
         The R10 conference will take place on Tuestday, June 27, 2017 in
         Vancouver,BC.
       </Text>
-      <Text>Code of Conduct</Text>
-      {allConducts.map(conduct => (
-        <CodeOfConduct key={conduct.id} conduct={conduct} />
-      ))}
-
-      <Text>&copy; Red Academy 2019</Text>
+      <Text style={styles.title}>Code of Conduct</Text>
+      <View style={styles.listItem}>
+        {allConducts.map(conduct => (
+          <CodeOfConduct key={conduct.id} conduct={conduct} />
+        ))}
+      </View>
+      <View style={styles.separator} />
+      <Text style={styles.footer}>&copy; Red Academy 2019</Text>
     </ScrollView>
   );
 };
