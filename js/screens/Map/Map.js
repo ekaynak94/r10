d829638fd1eb64c3dff6038ac3a10925626be247
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-31 11:20:40 -0700

    Return a text component instead of map view to prevent api key error

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./styles";
 import MapView from "react-native-maps";
+import { Text } from "react-native";
 
 class Map extends Component {
   constructor(props) {
@@ -21,11 +22,12 @@ class Map extends Component {
 
   render() {
     return (
-      <MapView
-        style={styles.map}
-        region={this.state.region}
-        onRegionChange={this.onRegionChange}
-      />
+      <Text>This is the map screen</Text>
+      // <MapView
+      //   style={styles.map}
+      //   region={this.state.region}
+      //   onRegionChange={this.onRegionChange}
+      // />
     );
   }
 }
