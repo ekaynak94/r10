1ee8e2a2cca65f7505d1f753a6c70972e5ca8149
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-31 12:04:14 -0700

    Set region and size map

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,35 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./styles";
 import MapView from "react-native-maps";
-import { Text } from "react-native";
 
-class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }
-    };
-  }
-
-  onRegionChange(region) {
-    this.setState({ region });
-  }
-
-  render() {
-    return (
-      <Text>This is the map screen</Text>
-      // <MapView
-      //   style={styles.map}
-      //   region={this.state.region}
-      //   onRegionChange={this.onRegionChange}
-      // />
-    );
-  }
-}
+const region = {
+  latitude: 49.264393,
+  longitude: -123.138066,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
+const Map = () => {
+  return <MapView style={styles.map} region={region} />;
+};
 
 export default Map;
--- js/screens/Map/styles.js
+++ js/screens/Map/styles.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from "react-native";
 import theme from "../../config/styles";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: "100%",
+    width: "100%"
+  }
+});
 
 export default styles;
