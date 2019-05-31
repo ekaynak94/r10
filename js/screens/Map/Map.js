5bf6ea90a7238ef2f68cf6853e84be4c295b6be3
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-31 11:16:33 -0700

    Link react native maps

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,10 +1,33 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Text, View } from "react-native";
+import React, { Component } from "react";
 import styles from "./styles";
+import MapView from "react-native-maps";
 
-const Map = () => {
-  return <View />;
-};
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <MapView
+        style={styles.map}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      />
+    );
+  }
+}
 
 export default Map;
