e56f98aead1cab4d7d0182fb8bea7ad1f3cb34aa
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-31 09:45:44 -0700

    Make component into a stateful component

--- js/components/CodeOfConduct/CodeOfConduct.js
+++ js/components/CodeOfConduct/CodeOfConduct.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { TouchableOpacity, View, Text } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
 
-const CodeOfConduct = ({ conduct }) => (
-  <View style={styles.item} key={(conduct, index) => conduct.id}>
-    <TouchableOpacity
-      onPress={() => {
-        console.log("open");
-      }}
-    >
-      <Text style={styles.itemTitle}>+ {conduct.title}</Text>
-    </TouchableOpacity>
-    <Text style={styles.itemDescription}>{conduct.description}</Text>
-  </View>
-);
+class CodeOfConduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  onClick = () => {
+    const isOpen = !this.state.isOpen;
+    this.setState({
+      isOpen
+    });
+  };
+
+  render() {
+    const { conduct } = this.props;
+    return (
+      <View style={styles.item} key={(conduct, index) => conduct.id}>
+        <TouchableOpacity onPress={this.onClick}>
+          <Text style={styles.itemTitle}>
+            {this.state.isOpen ? "-" : "+"} {conduct.title}
+          </Text>
+        </TouchableOpacity>
+        {this.state.isOpen && (
+          <Text style={styles.itemDescription}>{conduct.description}</Text>
+        )}
+      </View>
+    );
+  }
+}
 
 CodeOfConduct.propTypes = {
   conduct: PropTypes.array.isRequired
